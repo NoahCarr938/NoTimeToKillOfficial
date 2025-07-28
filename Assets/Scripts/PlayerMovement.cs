b9fdccb3fbb931f5c49f75a3c1d83d47506382b1
NoahCarr938 <noahcarrbusiness2759@gmail.com>
2025-07-28 18:14:21 -0500

    Working on getting the basics of the player input system set up.

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -1,18 +1,30 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Runtime.CompilerServices;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class PlayerMovement : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    [Header("Player Settings")]
+    [SerializeField] private float _moveSpeed = 10.0f;
+
+    // Standard float for horizontal movemepnt
+    private float _horizontal;
+    private Rigidbody _playerRB;
+
+    private void Start()
     {
-        
+        _playerRB = GetComponent<Rigidbody>();
     }
 
-    // Update is called once per frame
-    void Update()
+    private void FixedUpdate()
     {
-        
+        _playerRB.velocity = new Vector2(_horizontal * _moveSpeed, _playerRB.velocity.y);
+    }
+
+    public void Move(InputAction.CallbackContext context)
+    {
+        _horizontal = context.ReadValue<Vector2>().x;
     }
 }
