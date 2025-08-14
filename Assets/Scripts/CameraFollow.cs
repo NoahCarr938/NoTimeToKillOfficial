e8d7eeead2336c9feff87a08688b60d99e4ea164
NoahCarr938 <noahcarrbusiness2759@gmail.com>
2025-08-13 20:25:10 -0500

    Added in camera following, adjusted the player speed, and added in vertical movement.

--- Assets/Scripts/CameraFollow.cs
+++ Assets/Scripts/CameraFollow.cs
@@ -1,18 +1,32 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Runtime.CompilerServices;
 using UnityEngine;
 
 public class CameraFollow : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    private Vector3 _offset;
+
+    // The target that the camera is following
+    [SerializeField] private Transform _target;
+
+    // How smoothly the camera follows the player
+    [SerializeField] private float _smoothTime;
+
+    private Vector3 _currentVeloity = Vector3.zero;
+
+    // Getting the target
+    public Transform GetTarget { get { return _target; } }
+
+
+    private void Awake()
     {
-        
+        _offset = transform.position - _target.position;
     }
 
-    // Update is called once per frame
-    void Update()
+    private void FixedUpdate()
     {
-        
+        Vector3 targetPosition = _target.position + _offset;
+        transform.position = Vector3.SmoothDamp(transform.position, targetPosition, ref _currentVeloity, _smoothTime);
     }
 }
--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -9,22 +9,29 @@ public class PlayerMovement : MonoBehaviour
     [Header("Player Settings")]
     [SerializeField] private float _moveSpeed = 10.0f;
 
-    // Standard float for horizontal movemepnt
+    // Standard float for horizontal movement
     private float _horizontal;
+    // Standard float for up and down vertical movement along the z axis
+    private float _vertical;
+    // Reference to the rigidbody component on our player
     private Rigidbody _playerRB;
 
     private void Start()
     {
+        // Getting the rigidbody component on our player
         _playerRB = GetComponent<Rigidbody>();
     }
 
     private void FixedUpdate()
     {
-        _playerRB.velocity = new Vector2(_horizontal * _moveSpeed, _playerRB.velocity.y);
+        // Setting our rigidbody's velocity to our float multiplied by the movement speed.
+        _playerRB.velocity = new Vector3(_horizontal * _moveSpeed, 0, _vertical * _moveSpeed);
     }
 
     public void Move(InputAction.CallbackContext context)
     {
+        // Setting our floats to the controls
         _horizontal = context.ReadValue<Vector2>().x;
+        _vertical = context.ReadValue<Vector2>().y;
     }
 }
